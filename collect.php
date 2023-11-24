d90756ac570c5187dadba2a044359efecb5b6b7a
SongHongWei <767340267@qq.com>
2023-11-24 14:39:15 +0800

    Update collect.php

--- collect.php
+++ collect.php
@@ -10,8 +10,8 @@ function addHeader ($subscription, $subscriptionName) {
     $headerText = "#profile-title: base64:" . base64_encode($subscriptionName) . "
 #profile-update-interval: 1
 #subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
-#support-url: https://t.me/v2raycollector
-#profile-web-page-url: https://github.com/yebekhe/TelegramV2rayCollector
+#support-url: https://t.me/airport_free
+#profile-web-page-url: https://crazygeeky.com/yebekhe/
 
 ";
 
